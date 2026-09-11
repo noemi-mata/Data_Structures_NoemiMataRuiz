062ecef751ce6f5be061f76372c64424b2962e6e
Noemi Mata Ruiz <1225100341.nmr@gmail.com>
2026-09-11 14:43:18 -0600

    Actividad

--- CarritoDeCompras1.java
+++ CarritoDeCompras1.java
@@ -1,12 +1,12 @@
-public interface CarritoDeCompras1 <T>{
+import Articulo.java as Articulo;
+
+public interface CarritoDeCompras1 <T> extends Articulo{
 
         void agregar(T articulo);
         void pagar();
         void eliminar(T articulo);
         void cancelar();     
         void vaciarCarrito();
-        void buscarProducto();
-        
+        void buscarProducto(); 
 
 }
-
